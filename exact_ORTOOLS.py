bf69a1d2cee92ca9de9d3ae45723cce85ea4eac5
deanzedd <theanha2hvtls@gmail.com>
2026-06-07 15:15:18 +0700

    add detailed testcase :))

---
+++ exact_ORTOOLS.py
@@ -0,0 +1,14 @@
+import time
+import tracemalloc
+import sys
+from ortools.sat.python import cp_model
+
+
+def input(data):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()  
